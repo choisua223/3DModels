5bffdc32319b0c11415a96c9c6b9e15861317ceb
choisua223 <choisua396@gmail.com>
2026-06-12 17:19:19 +0900

    Add files via upload

---
+++ data/collection.js
@@ -0,0 +1,4 @@
+const collection=[
+{id:'roof-tile',name:'8엽연화문 와당',description:'백제 왕도 공주 시기의 대표 수막새',longDescription:'공산성 출토 8엽연화문 와당',thumbnail:'',model:'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Duck/glTF/Duck.gltf'},
+{id:'stone-buddha',name:'석조여래좌상',description:'공주 지역 불교문화 대표 유산',longDescription:'공주 지역 석조여래좌상',thumbnail:'',model:'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf'}
+];
